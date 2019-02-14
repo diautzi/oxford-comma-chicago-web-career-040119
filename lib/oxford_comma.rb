c250d62ff2ab8900bc02ed3f44a32c20bebcf002
Diana Utan <dia_utzi@yahoo.com>
2019-02-14 03:18:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
   if array.size == 1
   array.join
-else
-  if array.size == 2
+elsif array.size == 2
     array.join(" and ")
-  else 
+ else 
    array[0..-2].join(", ") + ", and " + array[-1]
-  end
   end
 end
